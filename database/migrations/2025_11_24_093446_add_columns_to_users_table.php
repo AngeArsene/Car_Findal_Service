d34d5d6635b354a15b28098210ec6c9e53b9ec3f
Ange Arsene <nkenmandenga@gmail.com>
2025-11-25 09:57:36 +0000

    Refactor user migration: change phone_number length to 16 characters; add car_types and fuel_types migrations with corresponding models and factories

--- database/migrations/2025_11_24_093446_add_columns_to_users_table.php
+++ database/migrations/2025_11_24_093446_add_columns_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone_number', 45)->after('email')->unique();
+            $table->string('phone_number', 16)->after('email')->unique();
             $table->string('google_id')->after('email_verified_at')->unique()->nullable();
             $table->string('facebook_id')->after('google_id')->unique()->nullable();
         });
