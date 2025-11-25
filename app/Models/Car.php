cd1c110ed038ab73687a3c11b4e7ced746d6c3b4
Ange Arsene <nkenmandenga@gmail.com>
2025-11-25 12:49:25 +0000

    Add models, factories, and migrations for Car, CarFeature, City, Neighborhood, and State; implement soft deletes for Car model

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Car extends Model
 {
     /** @use HasFactory<\Database\Factories\CarFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 }
---
+++ database/migrations/2025_11_25_115210_add_column_to_cars_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->after('id', function (Blueprint $table) {
+                $table->integer('year', unsigned: true);
+                $table->integer('price', unsigned: true);
+                $table->foreignId('neighborhood_id')->constrained('neighborhoods');
+                $table->string('vin', 255);
+                $table->integer('mileage', unsigned: true);
+                $table->longText('description')->nullable();
+                $table->foreignId('user_id')->constrained('users');
+                $table->foreignId('maker_id')->constrained('makers');
+                $table->foreignId('model_id')->constrained('models');
+                $table->foreignId('car_type_id')->constrained('car_types');
+                $table->foreignId('fuel_type_id')->constrained('fuel_types');
+                $table->foreignId('city_id')->constrained('cities');
+            });
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            //
+        });
+    }
+};
