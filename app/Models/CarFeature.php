861ffeee1213bf0e40decfadfd602cebaf7939fc
Ange Arsene <nkenmandenga@gmail.com>
2025-11-27 09:22:34 +0000

    Refactor CarFeature model to dynamically cast attributes as boolean based on database columns

--- app/Models/CarFeature.php
+++ app/Models/CarFeature.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CarFeature extends Model
 {
@@ -11,4 +12,24 @@ class CarFeature extends Model
     use HasFactory;
 
     public $timestamps = false;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        $columns = Schema::getColumnListing($this->getTable());
+
+        $casts = [];
+
+        foreach ($columns as $column) {
+            if ($column !== 'car_id') {
+                $casts[$column] = 'boolean';
+            }
+        }
+
+        return $casts;
+    }
 }
